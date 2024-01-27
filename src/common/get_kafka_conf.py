3de5b91cb0c38f959836a9d1f3e90954a9a662e3
rnimisha <nimisaraut@gmail.com>
2024-01-27 23:10:44 +0545

    feat: create kafka producer

--- src/common/get_kafka_conf.py
+++ src/common/get_kafka_conf.py
@@ -1,3 +1,22 @@
+import logging
+
+from confluent_kafka import Producer
+
+from src.config.settings import settings
+
+
 def get_kakfa_conf():
-    conf = {"bootstrap.servers": "localhost:9092"}
+    host = settings.KAFKA_HOST
+    port = settings.KAFKA_PORT
+    conf = {"bootstrap.servers": f"{host}:{port}"}
     return conf
+
+
+def create_kafka_producer() -> Producer:
+    try:
+        conf = get_kakfa_conf()
+        producer = Producer(conf)
+        logging.info("Kafka producer created successfully")
+        return producer
+    except Exception as e:
+        logging.error(f"Error creating kafka producer : {e}")
