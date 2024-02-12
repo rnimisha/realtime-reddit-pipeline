beb912e5a5b719504c17093ce51930316d8654b0
rnimisha <nimisaraut@gmail.com>
2024-02-12 22:31:53 +0545

    feat: add metrics card

---
+++ src/visualization/charts/metrics_card.py
@@ -0,0 +1,37 @@
+import streamlit
+from pandas import DataFrame
+from streamlit_extras.metric_cards import style_metric_cards
+
+
+def get_metrics_card(st: streamlit, df: DataFrame):
+    sentiment_counts = df["sentiment"].value_counts()
+
+    style_metric_cards(
+        background_color="#000",
+        border_left_color="#686664",
+        border_color="#000000",
+        box_shadow="#F71938",
+    )
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.success("Positive", icon="😄")
+        st.metric(
+            label="Positive",
+            value=f" {sentiment_counts.get('positive', 0)}",
+        )
+
+    with col2:
+        st.warning("Neutral", icon="😐")
+        st.metric(
+            label="Neutral",
+            value=f" {sentiment_counts.get('neutral', 0)}",
+        )
+
+    with col3:
+        st.error("Negative", icon="😡")
+        st.metric(
+            label="Negative",
+            value=f" {sentiment_counts.get('negative', 0)}",
+        )
