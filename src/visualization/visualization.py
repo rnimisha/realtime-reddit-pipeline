762df2fe4ae7b616f2e49698655bf8ffb252d147
rnimisha <nimisaraut@gmail.com>
2024-02-12 22:32:17 +0545

    feat: insert card in dashboard

--- src/visualization/visualization.py
+++ src/visualization/visualization.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 
 from src.config.settings import settings
+from src.visualization.charts.metrics_card import get_metrics_card
 from src.visualization.db.connect import init_connect
 
 # database
@@ -23,3 +24,14 @@ st.set_page_config(
     layout="wide",
 )
 st.title(":bar_chart: Samsung Dashboard")
+
+
+def create_dashboard(df: pd.DataFrame):
+    get_metrics_card(st, df)
+
+
+data = collection.find()
+df = pd.DataFrame(list(data))
+df["created_at"] = pd.to_datetime(df["created_at"])
+
+create_dashboard(df)
