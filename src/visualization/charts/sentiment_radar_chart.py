a05dcfc1b27219f66187f5b4c48b399e13984f5c
rnimisha <nimisaraut@gmail.com>
2024-02-13 11:10:42 +0545

    feat: add radar chart

---
+++ src/visualization/charts/sentiment_radar_chart.py
@@ -0,0 +1,33 @@
+import plotly.graph_objects as go
+from pandas import DataFrame
+
+
+def sentiment_radar_chart(data: DataFrame):
+    sentiments = data["sentiment"].unique()
+
+    counts = []
+    for sentiment in sentiments:
+        count = len(data[data["sentiment"] == sentiment])
+        counts.append(count)
+
+    fig = go.Figure(
+        data=go.Scatterpolar(
+            r=counts,
+            theta=sentiments,
+            fill="toself",
+            line=dict(color="yellow", width=2, shape="linear"),
+        )
+    )
+
+    fig.update_layout(
+        polar=dict(
+            bgcolor="black",
+            radialaxis=dict(visible=True),
+        ),
+        showlegend=False,
+        title=dict(
+            text="Sentiment Radar Chart",
+            font=dict(size=18, color="white"),
+        ),
+    )
+    return fig
